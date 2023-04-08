d0643359a2e908fdb136c6f4785ab07b300a5df2
Franco Silva <franco_tigres18@hotmail.com>
2023-04-08 11:51:43 -0600

    Vistas templates add

---
+++ views/templates/header.php
@@ -0,0 +1,17 @@
+<header class="header">
+  <div class="header__contenedor">
+
+    <div class="header__barra">
+      <h2 class="header__titulo">Estetica Unisex Silvia</h2>
+      <nav class="navegacion">
+        <a href="/login" class="navegacion__enlace">Iniciar Sesión</a>
+        <a href="/crear-cuenta" class="navegacion__enlace">Registrarse</a>
+      </nav>
+    </div>
+
+    <div class="header__texto">
+      <h1>Agenda tu cita, asi no tendrás que esperar</h1>
+    </div>
+  </div>
+
+</header>
