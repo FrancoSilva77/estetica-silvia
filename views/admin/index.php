42b0c50bed4b19702cfc8a6e2907f01f949188c0
Franco Silva <franco_tigres18@hotmail.com>
2023-04-25 08:25:28 -0600

    Vista Admin add

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,3 +1,50 @@
 <main class="admin contenedor">
 
+  <h2 class="main__heading">Panel de Andministración</h2>
+
+  <div class="cita__barra">
+    <p>Hola <?php echo $nombre  ?? ''; ?></p>
+    <a href="/logout" class="cita__sesion">Cerrar Sesión</a>
+  </div>
+
+  <div class="admin__busqueda">
+    <form class="formulario">
+      <div class="formulario__campo">
+        <label class="formulario__label" for="fecha">Fecha:</label>
+        <input class="formulario__input" type="date" id="fecha" name="fecha" value="<?php echo $fecha; ?>">
+      </div>
+    </form>
+  </div>
+
+  <?php if (count($citas) === 0) {
+    echo "<h2>No Hay Citas en este día</h2>";
+  } ?>
+
+  <div id="admin__citas">
+    <ul class="admin__lista-citas">
+
+      <?php
+      $idCita = 0;
+      foreach ($citas as $cita) :
+        if ($idCita !== $cita->id) :
+          $idCita = $cita->id;
+      ?>
+          <li class="admin__lista-cita">
+            <p class="admin__lista-descripcion">ID: <span><?php echo $cita->id; ?></span></p>
+            <p class="admin__lista-descripcion">Hora: <span><?php echo $cita->hora; ?></span></p>
+            <p class="admin__lista-descripcion">Cliente: <span><?php echo $cita->cliente; ?></span></p>
+            <p class="admin__lista-descripcion">Email: <span><?php echo $cita->email; ?></span></p>
+            <p class="admin__lista-descripcion">Telefono: <span><?php echo $cita->telefono; ?></span></p>
+            <p class="admin__lista-descripcion">Servicio: <span><?php echo $cita->servicio; ?></span></p>
+            <p class="admin__lista-descripcion">Precio: <span><?php echo '$' . $cita->precio; ?></span></p>
+
+          <?php endif; ?>
+          </li>
+        <?php endforeach; ?>
+    </ul>
+  </div>
 </main>
+
+<?php
+$script = "<script src='build/js/buscador.js'></script>"
+?>
