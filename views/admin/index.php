b4e414df58a5bda2b9f1cbdfe75328e61eb47487
Franco Silva <franco_tigres18@hotmail.com>
2023-04-26 09:33:59 -0600

    Funcionalidad admin add

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,10 +1,16 @@
-<main class="admin contenedor">
+<main class="admin">
 
   <h2 class="main__heading">Panel de Andministración</h2>
 
   <div class="cita__barra">
     <p>Hola <?php echo $nombre  ?? ''; ?></p>
     <a href="/logout" class="cita__sesion">Cerrar Sesión</a>
+  </div>
+
+  <div class="admin__barra">
+    <a class="admin__barra-enlace" href="/admin">Ver Citas</a>
+    <a class="admin__barra-enlace" href="/servicios">Ver Servicios</a>
+    <a class="admin__barra-enlace" href="/servicios/crear">Crear Nuevo Servicio</a>
   </div>
 
   <div class="admin__busqueda">
