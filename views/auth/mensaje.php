5816b823e82352fe322bbf44ab589e8a1ae02d06
Franco Silva <franco_tigres18@hotmail.com>
2023-05-10 08:57:58 -0600

    Titulo mensaje add

--- views/auth/mensaje.php
+++ views/auth/mensaje.php
@@ -1,4 +1,4 @@
 <main class="auth">
-  <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+  <h2 class="auth__heading">Cuenta creada</h2>
   <p class="auth__texto">Es necesario confirmar tu cuenta, revisa tu bandeja de entrada para hacerlo</p>
 </main>
