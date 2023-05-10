b99fefa7645a23c3e0df18a9497bb65f018a82e3
Franco Silva <franco_tigres18@hotmail.com>
2023-05-10 08:34:19 -0600

    Modificacion de url para emails

--- classes/Email.php
+++ classes/Email.php
@@ -41,7 +41,7 @@ class Email
 
     $contenido = "<html>";
     $contenido .= "<p><strong>Hola " . $this->nombre . " </strong> Has creado tu cuenta en Estetica Silvia, solo debes confirmarla presionando el siguiente enlace</p>";
-    $contenido .= "<p>Presiona aquí: <a href='https://esteticasilvia.alwaysdata.net/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";
+    $contenido .= "<p>Presiona aquí: <a href='https://esteticasiliviaa.alwaysdata.net/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";
     $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
     $contenido .= "</html>";
 
@@ -73,7 +73,7 @@ class Email
 
     $contenido = "<html>";
     $contenido .= "<p><strong>Hola " . $this->nombre . " </strong> Has solicitado reestablecer tu contraseña, sigue el siguiente enlace para hacerlo.</p>";
-    $contenido .= "<p>Presiona aquí: <a href='https://esteticasilvia.alwaysdata.net/recuperar?token=" . $this->token . "'>Reestablecer Contaseña</a>";
+    $contenido .= "<p>Presiona aquí: <a href='https://esteticasiliviaa.alwaysdata.net/recuperar?token=" . $this->token . "'>Reestablecer Contaseña</a>";
     $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
     $contenido .= "</html>";
 
