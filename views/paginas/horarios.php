4188cfb80d3088405a25f58de738a1fae809e3de
Franco Silva <franco_tigres18@hotmail.com>
2023-05-20 08:06:18 -0600

    Horarios update

--- views/paginas/horarios.php
+++ views/paginas/horarios.php
@@ -16,37 +16,37 @@
           <tr class="tabla__tr">
             <td class="tabla__td">Lunes</td>
             <td class="tabla__td">10:30</td>
-            <td class="tabla__td">21:00</td>
+            <td class="tabla__td">20:30</td>
           </tr>
 
           <tr class="tabla__tr">
             <td class="tabla__td">Martes</td>
             <td class="tabla__td">10:30</td>
-            <td class="tabla__td">21:00</td>
+            <td class="tabla__td">20:30</td>
           </tr>
 
           <tr class="tabla__tr">
             <td class="tabla__td">Miércoles</td>
             <td class="tabla__td">10:30</td>
-            <td class="tabla__td">21:00</td>
+            <td class="tabla__td">20:30</td>
           </tr>
 
           <tr class="tabla__tr">
             <td class="tabla__td">Jueves</td>
             <td class="tabla__td">10:30</td>
-            <td class="tabla__td">21:00</td>
+            <td class="tabla__td">20:30</td>
           </tr>
 
           <tr class="tabla__tr">
             <td class="tabla__td">Viernes</td>
             <td class="tabla__td">10:30</td>
-            <td class="tabla__td">21:00</td>
+            <td class="tabla__td">20:30</td>
           </tr>
 
           <tr class="tabla__tr">
             <td class="tabla__td">Sábado</td>
             <td class="tabla__td">10:30</td>
-            <td class="tabla__td">21:00</td>
+            <td class="tabla__td">20:30</td>
           </tr>
 
           <tr class="tabla__tr">
